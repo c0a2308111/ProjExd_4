aa648e43d12e237836e1c12d2721fe757f38f150
nakata <c0b2310742@edu.teu.ac.jp>
2024-12-10 15:22:20 +0900

    追加機能6:完成

--- musou_kokaton.py
+++ musou_kokaton.py
@@ -141,20 +141,21 @@ class Beam(pg.sprite.Sprite):
     """
     ビームに関するクラス
     """
-    def __init__(self, bird: Bird):
+    def __init__(self, bird: Bird, angle0: float = 0):  # 角度の引数angle0を追加
         """
         ビーム画像Surfaceを生成する
         引数 bird：ビームを放つこうかとん
+        引数 angle0：ビームの回転角度
         """
         super().__init__()
         self.vx, self.vy = bird.dire
-        angle = math.degrees(math.atan2(-self.vy, self.vx))
+        angle = math.degrees(math.atan2(-self.vy, self.vx)) + angle0  # 角度を加算
         self.image = pg.transform.rotozoom(pg.image.load(f"fig/beam.png"), angle, 1.0)
         self.vx = math.cos(math.radians(angle))
         self.vy = -math.sin(math.radians(angle))
         self.rect = self.image.get_rect()
-        self.rect.centery = bird.rect.centery+bird.rect.height*self.vy
-        self.rect.centerx = bird.rect.centerx+bird.rect.width*self.vx
+        self.rect.centery = bird.rect.centery + bird.rect.height * self.vy
+        self.rect.centerx = bird.rect.centerx + bird.rect.width * self.vx
         self.speed = 10
 
     def update(self):
@@ -162,9 +163,34 @@ class Beam(pg.sprite.Sprite):
         ビームを速度ベクトルself.vx, self.vyに基づき移動させる
         引数 screen：画面Surface
         """
-        self.rect.move_ip(self.speed*self.vx, self.speed*self.vy)
+        self.rect.move_ip(self.speed * self.vx, self.speed * self.vy)
         if check_bound(self.rect) != (True, True):
             self.kill()
+
+
+class NeoBeam:
+    """
+    弾幕ビームを発射するクラス
+    """
+    def __init__(self, bird: Bird, num: int = 5):
+        """
+        弾幕のビームを生成する
+        引数1 bird：ビームを放つこうかとん
+        引数2 num：ビームの発射数
+        """
+        self.beams = self.gen_beams(bird, num)
+
+    def gen_beams(self, bird: Bird, num: int) -> list[Beam]:
+        """
+        指定されたビーム数に応じて、-50度から+50度の範囲でビームを発射する
+        引数1 bird：ビームを放つこうかとん
+        引数2 num：ビームの発射数
+        戻り値：ビームのリスト
+        """
+        step = 100 // (num - 1) if num > 1 else 0  # 角度のステップ計算
+        angles = list(range(-50, 51, step))  # 角度のリスト
+        return [Beam(bird, angle) for angle in angles]
+
 
 
 class Explosion(pg.sprite.Sprite):
@@ -261,8 +287,14 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return 0
-            if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
-                beams.add(Beam(bird))
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    if key_lst[pg.K_LSHIFT]:  # 左Shift + スペースの同時押しで弾幕発動
+                        neobeam = NeoBeam(bird, 5)  # 5方向の弾幕を発射
+                        beams.add(*neobeam.beams)  # ビームグループにビームを追加
+                    else:
+                        beams.add(Beam(bird))  # 通常のビーム発射
+
         screen.blit(bg_img, [0, 0])
 
         if tmr%200 == 0:  # 200フレームに1回，敵機を出現させる
